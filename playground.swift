6364cd9ca84964584aafc43b69e7826f7a91bf5c
heyjuan90 <84816147+heyjuan90@users.noreply.github.com>
2021-05-25 19:36:56 -0500

    the main code
    code

---
+++ playground.swift
@@ -0,0 +1,12 @@
+/// stocks ///
+func end() {
+    print("thats the stocks")
+}
+print("stocks may 24")
+print("dow jones 34,393.98")
+print("s&p 500 4,197.05")
+print("aapl 127.10")
+print("ba 237.44")
+print("brk-b 289.73")
+print("dis 174.31")
+end()
