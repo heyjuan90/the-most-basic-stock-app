93edb13b56fecbdd2e8fc6adfade62bd151a617e
heyjuan90 <84816147+heyjuan90@users.noreply.github.com>
2021-05-29 11:55:27 -0500

    new code 2
    new code

---
+++ code.swift
@@ -0,0 +1,12 @@
+/// stocks ///
+func end() {
+    print("thats the stocks")
+}
+print("stocks may 29")
+print("dow jones 34,529.45")
+print("s&p 500 4,204.11")
+print("aapl 124.61")
+print("ba 247.02")
+print("brk-b 289.44")
+print("dis 178.65")
+end()
