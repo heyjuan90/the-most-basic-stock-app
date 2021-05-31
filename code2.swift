dce2c616c5a9085595a7441f27adb0c57044e048
heyjuan90 <84816147+heyjuan90@users.noreply.github.com>
2021-05-31 09:45:38 -0500

    code2.swift
    CODE

---
+++ code2.swift
@@ -0,0 +1,15 @@
+// stocks ///
+func end() {
+    print("thats the stocks")
+}
+func start() {
+    print("thats the stooks for today")
+}
+start()
+print("dow jones 34,529.45")
+print("s&p 500 4,204.11")
+print("aapl 124.61")
+print("ba 247.02")
+print("brk-b 289.44")
+print("dis 178.65")
+end()
